652c680c8acadf359cfb33b1d45788364e79b931
Ryan <ryanbiy01@gmail.com>
2023-07-29 12:50:41 +0300

    Added homepage video

--- src/components/LandingPage/Home.jsx
+++ src/components/LandingPage/Home.jsx
@@ -1,10 +1,12 @@
 import Carousel from "./Carousel";
+import Carousel2 from "./Carousel2"
 import Products from "./Products";
 
 const Home = () => {
     return (
         <div>
             <Carousel/>
+            <Carousel2/>
             <Products />
         </div>
     );
