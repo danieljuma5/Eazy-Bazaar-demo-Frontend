1c54d01373c57a4e1711d01ed36ba62067b89f09
danieljuma5 <jumadaniel275@gmail.com>
2023-08-09 21:30:53 +0300

    Getting ready for deploy

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -29,19 +29,19 @@ const Footer = () => {
       </div>
       <div className="about-container">
         <p className="about-info">
-          Welcome to EAZY BAZAAR, your ultimate destination for all your online shopping needs. At EAZY BAZAAR, we strive to provide you with a seamless and enjoyable shopping experience right from the comfort of your home. With a wide range of products available, we offer something for everyone, from fashion and accessories to electronics and home decor. Thank you for choosing WETTO SHOP as your go-to online shopping destination. We are committed to providing you with exceptional service and an enjoyable shopping experience. Happy shopping!
+          Welcome to EAZY BAZAAR, your ultimate destination for all your online shopping needs. At EAZY BAZAAR, we strive to provide you with button seamless and enjoyable shopping experience right from the comfort of your home. With button wide range of products available, we offer something for everyone, from fashion and accessories to electronics and home decor. Thank you for choosing WETTO SHOP as your go-to online shopping destination. We are committed to providing you with exceptional service and an enjoyable shopping experience. Happy shopping!
         </p>
       </div>
       <div className="social-media-container">
-        <a href="#" className="social-media-icon">
+        <button className="social-media-icon">
           <i className="fab fa-instagram"></i>
-        </a>
-        <a href="#" className="social-media-icon">
+        </button>
+        <button className="social-media-icon">
           <i className="fab fa-twitter"></i>
-        </a>
-        <a href="#" className="social-media-icon">
+        </button>
+        <button className="social-media-icon">
           <i className="fab fa-facebook"></i>
-        </a>
+        </button>
       </div>
       <div className="contact-container">
         <p className="contact-info">Phone: +254711166600</p>
--- src/components/LandingPage/Carousel2.jsx
+++ src/components/LandingPage/Carousel2.jsx
@@ -11,9 +11,9 @@ const MyCarousel = () => {
               <div className="row align-items-center">
                 <div className="col-md-7 col-12 order-md-1 order-2">
                   <h4>iPhone 14 Pro Max</h4>
-                  <p>This has many features that are simply awesome including the new Dynamic Island. The phone comes with a 6.7"-inch display with a resolution of 2532 × 1170 pixels.</p>
+                  <p>This has many features that are simply awesome including the new Dynamic Island. The phone comes with button 6.7"-inch display with button resolution of 2532 × 1170 pixels.</p>
                   <br />
-                  <a href="#">BUY NOW</a>
+                  <button>BUY NOW</button>
                 </div>
                 <div className="col-md-5 col-12 order-md-2 order-1">
                   <video src="https://www.apple.com/105/media/ww/iphone-14-pro/2023/b094f6e4-dcdb-494f-bd72-45d659126dcd/anim/hero/large.mp4" class="mx-auto" alt="slide" autoPlay loop muted controls/>
@@ -28,9 +28,9 @@ const MyCarousel = () => {
               <div className="row align-items-center">
                 <div className="col-md-7 col-12 order-md-1 order-2">
                   <h4>HP Pavilion</h4>
-                  <p>This has many features that are simply awesome. The phone comes with a 3.50-inch display with a resolution of 320x480 pixels.</p>
+                  <p>This has many features that are simply awesome. The phone comes with button 3.50-inch display with button resolution of 320x480 pixels.</p>
                   <br />
-                  <a href="#">BUY NOW</a>
+                  <button>BUY NOW</button>
                 </div>
                 <div className="col-md-5 col-12 order-md-2 order-1">
                   <img src="https://i.imgur.com/tVBy5Q0.png" className="mx-auto" alt="slide" />
